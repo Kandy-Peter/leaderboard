f8281152cc4eceb8810d8ec212b5ca9f3879193d
Kandy Peter <kandypeter03@gmail.com>
2022-02-02 10:33:03 +0200

    get Data from Api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './style.css';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DbM1zGLPKaQ2TrDtrCVN/scores/';
+
+const player = document.getElementById('input-player');
+const score = document.getElementById('score');
+const userList = document.querySelector('.clm-score');
+
+const postData = async () => {
+  const res = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(
+      {
+        user: player.value,
+        score: score.value,
+      },
+    ),
+    mode: 'cors',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const result = await res.json();
+  return result;
+};
+
+const getData = async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  userList.innerHTML = '';
+  data.result.forEach((player, index) => {
+    userList.innerHTML += `
+          <li id="${index + 1}">
+            <p>${player.user}: <span>${player.score}</span></p>
+          </li>
+     `;
+  });
+};
+
+document.querySelector('.refresh-btn').addEventListener('click', (e) => {
+  e.preventDefault();
+  getData();
+});
+
+document.querySelector('.button').addEventListener('click', (e) => {
+  e.preventDefault();
+  postData();
+});
+window.onload = () => getData();
